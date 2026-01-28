7d1d2eaf2702c81983471213d69afe5550592280
Komal-Mathur <91676569+Komal-Mathur@users.noreply.github.com>
2026-01-28 14:31:44 +0530

    Force disable flat config and set standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Disables the entire linting step during the build process
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // Disables type checking during the build process
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
+  // Ensures the build doesn't fail on "Collecting build traces"
+  // if there's a minor version mismatch in the environment
+  output: 'standalone', 
 };
 
 export default nextConfig;
